d48d0e2cb9283ec50f0932c65670528a79aa8351
Suvojit Mondal <mondalsuvojit160@gmail.com>
2025-12-09 19:30:06 +0530

    console row data

--- src/pages/Dashboard/ShowQuery.tsx
+++ src/pages/Dashboard/ShowQuery.tsx
@@ -41,7 +41,8 @@ const ShowQuery = () => {
   }, [user]);
 
   // Outside component
-  const handleEditClick = (navigate) => {
+  const handleEditClick = (navigate, row) => {
+    console.log("Clicked row from showquery:", row);
     navigate("/layout/query-designer");
   };
 
@@ -110,7 +111,7 @@ const ShowQuery = () => {
                   <td className="px-6 py-4 text-gray-600">{query.rows_effected}</td>
                   <td className="px-6 py-4">
                     <button className="text-green-600 bg-green-100 px-3 py-1 rounded-full text-xs font-medium hover:bg-green-200"
-                      onClick={() => handleEditClick(navigate)} >
+                      onClick={() => handleEditClick(navigate, query)} >
                       Edit
                     </button>
                   </td>
