f4f22ca2f2cf3b5224ce372258160a7193ff1862
PSpabitra <sarkarpabi42@gmail.com>
2025-12-20 13:54:52 +0530

    unused_feaures_comment out

--- src/pages/Setting/components/ProfileSettings.tsx
+++ src/pages/Setting/components/ProfileSettings.tsx
@@ -77,7 +77,7 @@ const ProfileSettings: React.FC<ProfileSettingsProps> = ({ activeTab }) => {
   return (
     <div className="max-w-8xl">
       <form onSubmit={formik.handleSubmit} className="relative">
-        <div className="absolute top-6 right-6">
+        {/* <div className="absolute top-6 right-6">
           <button
             type="submit"
             disabled={isSaving}
@@ -86,7 +86,7 @@ const ProfileSettings: React.FC<ProfileSettingsProps> = ({ activeTab }) => {
             <Save size={18} />
             {isSaving ? 'Saving...' : 'Save Changes'}
           </button>
-        </div>
+        </div> */}
         <div className="rounded-xl px-4 ">
           <div className="flex flex-col md:flex-row items-start md:items-center ">
             <div className="relative p-4 ">
@@ -111,7 +111,7 @@ const ProfileSettings: React.FC<ProfileSettingsProps> = ({ activeTab }) => {
             </div>
             <div className="flex-1">
               <h3 className="text-lg font-semibold text-gray-800 mb-2">{userData?.company_name || 'Company Name'}</h3>
-              <div className="flex flex-wrap gap-3">
+              {/* <div className="flex flex-wrap gap-3">
                 <label
                   htmlFor="avatar-upload"
                   className="px-4 py-2 bg-blue-50 text-blue-600 rounded-lg hover:bg-blue-100 cursor-pointer transition-colors flex items-center gap-2"
@@ -127,7 +127,7 @@ const ProfileSettings: React.FC<ProfileSettingsProps> = ({ activeTab }) => {
                   <Trash2 size={18} />
                   Delete avatar
                 </button>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
