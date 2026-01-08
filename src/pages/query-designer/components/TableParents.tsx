ea57b84700b65be2c897fe664555b923383fd2e6
PSpabitra <sarkarpabi42@gmail.com>
2026-01-08 15:57:01 +0530

    insights text align

--- src/pages/query-designer/components/TableParents.tsx
+++ src/pages/query-designer/components/TableParents.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import AutorenewRoundedIcon from "@mui/icons-material/AutorenewRounded";
 import { useTheme } from "../../../theme";
 import ProductDataTable from "./DataTable";
@@ -16,13 +16,24 @@ interface DashboardTableProps {
   viewSelection: string;
   isLoading?: boolean;
 }
-export default function TableParents({ data, columns, insights, globalFilter, tableName, viewSelection, isLoading }: DashboardTableProps) {
+export default function TableParents({
+  data,
+  columns,
+  insights,
+  globalFilter,
+  tableName,
+  viewSelection,
+  isLoading,
+}: DashboardTableProps) {
   const { theme } = useTheme();
   const [isExpanded, setIsExpanded] = useState(true);
 
   const filteredColumns = columns
-    .filter((col, index, self) => index === self.findIndex((t) => t.column_name === col.column_name))
-    .filter(col => col.column_name !== 'row_hash');
+    .filter(
+      (col, index, self) =>
+        index === self.findIndex((t) => t.column_name === col.column_name)
+    )
+    .filter((col) => col.column_name !== "row_hash");
 
   if (isLoading) {
     return (
@@ -45,35 +56,44 @@ export default function TableParents({ data, columns, insights, globalFilter, ta
 
   return (
     <div className="px-4">
-      <div className="rounded-xl shadow-xs pb-4" >
+      <div className="rounded-xl shadow-xs pb-4">
         {isExpanded && (
           <>
-            {viewSelection === 'metadata' && (
-              
+            {viewSelection === "metadata" && (
               <div className="p-4 min-h-[200px]  max-h-[200px]flex items-center justify-center custom-table mb-5">
                 <div className="flex flex-wrap gap-3">
-                  {filteredColumns.map(col => (
-                    <span key={col.column_name} className="px-5 py-2 bg-gray-200 text-gray-700 rounded-full text-sm">
+                  {filteredColumns.map((col) => (
+                    <span
+                      key={col.column_name}
+                      className="px-5 py-2 bg-gray-200 text-gray-700 rounded-full text-sm"
+                    >
                       {col.column_name}
                     </span>
                   ))}
                 </div>
               </div>
             )}
-            {viewSelection === 'dataview' && (
-              <ProductDataTable data={data} globalFilter={globalFilter} columns={filteredColumns} />
+            {viewSelection === "dataview" && (
+              <ProductDataTable
+                data={data}
+                globalFilter={globalFilter}
+                columns={filteredColumns}
+              />
             )}
-            {viewSelection === 'insights' && (
+            {viewSelection === "insights" && (
               <div className="custom-table mb-5">
-              <div className="p-4 min-h-[200px] max-h-[200px] overflow-y-auto">
-                <ul className="list-disc list-inside space-y-2">
-                  {insights.map((insight, index) => (
-                    <li key={index} className="text-sm text-gray-700">
-                      {insight}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+                <div className="p-4 min-h-[200px] max-h-[200px] overflow-y-auto">
+                  <ul className="list-disc list-outside pl-5 space-y-2">
+                    {insights.map((insight, index) => (
+                      <li
+                        key={index}
+                        className="text-sm text-gray-700 leading-relaxed"
+                      >
+                        {insight}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             )}
           </>
