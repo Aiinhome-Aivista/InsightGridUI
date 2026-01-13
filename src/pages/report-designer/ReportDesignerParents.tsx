2a06c987b24f5beddc55369bab65c4a93fd36eab
PSpabitra <sarkarpabi42@gmail.com>
2026-01-13 13:07:42 +0530

    BG REMOVED FOR

--- src/pages/report-designer/ReportDesignerParents.tsx
+++ src/pages/report-designer/ReportDesignerParents.tsx
@@ -299,7 +299,7 @@ export default function TableView() {
   // }, [selectedAggregations])
 
   return (
-    <div className="flex flex-col rounded-xl m-4 max-w-screen overflow-hidden">
+    <div className="flex flex-col rounded-xl m-4 w-full overflow-hidden">
       <DataViewHeader
         globalFilter={globalFilter}
         setGlobalFilter={setGlobalFilter}
