0bffe8a7747096ea4601b9be66b3c47af45993bc
Suvojit Mondal <mondalsuvojit160@gmail.com>
2026-01-16 19:32:36 +0530

    add facebook

--- src/pages/LandingPage/components/LandingFooter.tsx
+++ src/pages/LandingPage/components/LandingFooter.tsx
@@ -58,7 +58,7 @@ export default function LandingFooter() {
               <div className="flex items-center gap-4">
                 {/* Facebook */}
                 <a
-                  href="#"
+                  href="https://www.facebook.com/sahajinsight"
                   className="w-9 h-9 flex items-center justify-center rounded-lg bg-slate-100 hover:bg-[#1877F2]/10 transition"
                 >
                   <svg
@@ -71,7 +71,7 @@ export default function LandingFooter() {
                 </a>
 
                 {/* Instagram */}
-                <a
+                {/* <a
                   href="#"
                   className="w-9 h-9 flex items-center justify-center rounded-lg bg-slate-100 hover:bg-pink-500/10 transition"
                 >
@@ -83,10 +83,10 @@ export default function LandingFooter() {
                     <path d="M7 2C4.2 2 2 4.2 2 7v10c0 2.8 2.2 5 5 5h10c2.8 0 5-2.2 5-5V7c0-2.8-2.2-5-5-5H7zm10 2c1.7 0 3 1.3 3 3v10c0 1.7-1.3 3-3 3H7c-1.7 0-3-1.3-3-3V7c0-1.7 1.3-3 3-3h10z" />
                     <path d="M12 7a5 5 0 100 10 5 5 0 000-10zm0 2a3 3 0 110 6 3 3 0 010-6z" />
                   </svg>
-                </a>
+                </a> */}
 
                 {/* X / Twitter */}
-                <a
+                {/* <a
                   href="#"
                   className="w-9 h-9 flex items-center justify-center rounded-lg bg-slate-100 hover:bg-black/10 transition"
                 >
@@ -97,10 +97,10 @@ export default function LandingFooter() {
                   >
                     <path d="M18.9 2H22l-7.5 8.6L23 22h-6.8l-5.3-6.7L5.7 22H2.6l8-9.2L1 2h7l4.8 6.1L18.9 2z" />
                   </svg>
-                </a>
+                </a> */}
 
                 {/* LinkedIn */}
-                <a
+                {/* <a
                   href="#"
                   className="w-9 h-9 flex items-center justify-center rounded-lg bg-slate-100 hover:bg-[#0A66C2]/10 transition"
                 >
@@ -111,7 +111,7 @@ export default function LandingFooter() {
                   >
                     <path d="M4.98 3.5a2.5 2.5 0 11-.01 5.01 2.5 2.5 0 01.01-5.01zM3 8.98h4v12H3zM9 8.98h3.8v1.64h.05c.53-1 1.82-2.05 3.75-2.05 4 0 4.74 2.63 4.74 6.05v6.36h-4v-5.64c0-1.35-.03-3.09-1.88-3.09-1.88 0-2.17 1.47-2.17 3v5.73H9z" />
                   </svg>
-                </a>
+                </a> */}
               </div>
             </div>
 
